19f6c714c0d2179c0107e53b2faaa1f776a296c9
emilykamiti <emilykamiti@gmail.com>
2023-10-28 13:28:30 +0300

    Inheritance and Liskov Substitution Principle
    Implemented Liskov Substitution Principle

--- src/main/java/com/concepts/oop/App.java
+++ src/main/java/com/concepts/oop/App.java
@@ -1,22 +1,31 @@
 package com.concepts.oop;
 
-
+import com.concepts.oop.oop.Animal;
 import com.concepts.oop.oop.Fish;
 import com.concepts.oop.oop.Hypopotamous;
 import com.concepts.oop.oop.Panther;
+import java.util.ArrayList;
+import java.util.List;
 
-public class App 
-{
-    public static void main( String[] args ){
-        Fish fish = new Fish();
+public class App {
+    public static void main(String[] args) {
+        Animal fish = new Fish();
         fish.move();
 
-        Hypopotamous hypo = new Hypopotamous();
+        Animal hypo = new Hypopotamous();
         hypo.move();
 
-        Panther panther = new Panther();
+        Animal panther = new Panther();
         panther.move();
 
-        
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Hypopotamous());
+        animals.add(new Panther());
+        animals.add(new Fish());
+
+        for (Animal animal : animals)
+        animal.eat();
+        //animal.rest();
+
     }
 }
--- src/main/java/com/concepts/oop/oop/Hypopotamous.java
+++ src/main/java/com/concepts/oop/oop/Hypopotamous.java
@@ -3,8 +3,12 @@ package com.concepts.oop.oop;
 public class Hypopotamous extends Animal {
 
     @Override
-    public void move(){
+    public void move() {
         System.out.println("Hypos: Move in water & land");
     }
 
+    @Override
+    public void eat() {
+        System.out.println("Hypos: Are harbivores");
+    }
 }
