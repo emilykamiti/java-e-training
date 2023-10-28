32f58069e713fea98abc968d9a9fa4f3683f0ec3
emilykamiti <emilykamiti@gmail.com>
2023-10-28 12:46:09 +0300

    Method Overloading Implemented method overloading: Run time polymorphism

--- src/main/java/com/concepts/oop/oop/Fish.java
+++ src/main/java/com/concepts/oop/oop/Fish.java
@@ -1,5 +1,10 @@
 package com.concepts.oop.oop;
 
 public class Fish extends Animal {
-    
+
+    @Override
+    public void move(){
+        System.out.println("Fish: Move on water");
+    }
+
 }
--- src/main/java/com/concepts/oop/oop/Hypopotamous.java
+++ src/main/java/com/concepts/oop/oop/Hypopotamous.java
@@ -1,5 +1,10 @@
 package com.concepts.oop.oop;
 
 public class Hypopotamous extends Animal {
-    
+
+    @Override
+    public void move(){
+        System.out.println("Hypos: Move in water & land");
+    }
+
 }
